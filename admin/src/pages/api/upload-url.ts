6603abf9e9eaa58b804aae20aabc924743055762
Olamilekan <nunuolamilekan@gmail.com>
2022-11-13 02:21:55 +0100

    file upload (2)

--- admin/src/pages/api/upload-url.ts
+++ admin/src/pages/api/upload-url.ts
@@ -7,6 +7,7 @@ import {
 } from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { join } from "path";
+import stream from "stream";
 import { format } from "util";
 import dataUrlToFile from "../../helper/dataUrlToFile";
 
@@ -22,40 +23,6 @@ const storage = new Storage({
 });
 
 const bucket = storage.bucket(process.env.BUCKET_NAME!);
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-//   try {
-//     const storage = new Storage({
-//       projectId: process.env.PROJECT_ID,
-//       token: process.env.TOKEN,
-//       credentials: {
-//         client_id: process.env.CLIENT_ID,
-//         client_email: process.env.CLIENT_EMAIL,
-//         private_key: process.env.PRIVATE_KEY,
-//         token_url: process.env.TOKEN_URL,
-//       },
-//     });
-
-//     const bucket = storage.bucket(process.env.BUCKET_NAME!);
-//     const file = bucket.file(req.query.file?.toString()!);
-
-//     const publicUrl = format(
-//       `https://storage.googleapis.com/${bucket.name}/${file.name}`
-//     );
-
-//     const [response] = await file.makePublic();
-
-//     console.log(response)
-//     res.status(200).json({
-//       url: publicUrl,
-//       name: req.query.file
-//     });
-//   } catch (error) {
-//     console.log(error)
-//   }
-
-// }
 
 const createFile = async (file: any, filepath: string) =>
   new Promise(async (resolve, reject) => {
@@ -97,6 +64,46 @@ const uploadFile = (filepath: string, filename: string) =>
       });
   });
 
+const createFileStream = (file: any) =>
+  new Promise((resolve, reject) => {
+    const blob = bucket.file(file.fileName);
+
+    var bufferStream = new stream.PassThrough();
+    bufferStream.end(Buffer.from(file.dataUrl, "base64"));
+    bufferStream
+      .pipe(
+        blob.createWriteStream({
+          metadata: {
+            contentType: file.mimeType,
+            metadata: {
+              custom: "metadata",
+            },
+          },
+          public: true,
+          validation: "md5",
+        })
+      )
+      .on("error",(err) => {
+        reject(err)
+        console.log(err)
+      })
+      .on("finish", async () => {
+        const publicUrl = format(
+          `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+        );
+
+        try {
+          await bucket.file(file.fileName).makePublic();
+        } catch {
+          console.log(
+            `Uploaded the file successfully: ${file.fileName}, but public access is denied!`
+          );
+        }
+        console.log(publicUrl);
+        resolve({ url: publicUrl, name: file.fileName });
+      });
+  });
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -113,21 +120,23 @@ export default async function handler(
   try {
     let file = req.body.file;
 
-    const filepath = join(
-      process.env.ROOT_DIR || process.cwd(),
-      `/public/uploads/${file.fileName}`
-    );
+    // const filepath = join(
+    //   process.env.ROOT_DIR || process.cwd(),
+    //   `/public/uploads/${file.fileName}`
+    // );
 
-    if (!existsSync(filepath)) {
-      const data = await createFile(file, filepath);
-      console.log(data);
-    }
+    const data = await createFileStream(file);
 
-    const data = await uploadFile(filepath, file.fileName);
+    // if (!existsSync(filepath)) {
+    //   const data = await createFile(file, filepath);
+    //   console.log(data);
+    // }
 
-    if (data) {
-      unlinkSync(filepath);
-    }
+    // const data = await uploadFile(filepath, file.fileName);
+
+    // if (data) {
+    //   unlinkSync(filepath);
+    // }
 
     res.status(200).json(data);
   } catch (e: any) {
