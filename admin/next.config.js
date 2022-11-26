738cb3714de87b3fff1ace15e3ce65658bf93928
Olamilekan <nunuolamilekan@gmail.com>
2022-11-26 03:21:37 +0100

    add vercel config

--- admin/next.config.js
+++ admin/next.config.js
@@ -3,12 +3,6 @@
 module.exports = {
   env: {
     SERVER_URL: process.env.SERVER_URL,
-    PROJECT_ID: process.env.PROJECT_ID,
-    TOKEN: process.env.TOKEN,
-    TOKEN_URL: process.env.TOKEN_URL,
-    PRIVATE_KEY: process.env.PRIVATE_KEY,
-    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
-    CLIENT_ID: process.env.CLIENT_ID,
   },
   reactStrictMode: true,
 };
