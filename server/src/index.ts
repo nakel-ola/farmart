938d54d0842e21c5be7c82373ae7307d03166421
Olamilekan <nunuolamilekan@gmail.com>
2023-03-16 01:58:02 +0100

    fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,9 +29,9 @@ const redisStore = new RedisStore({
 async function bootstrap() {
   const app = express();
 
-  app.use(cors);
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: false }));
+  app.use(cors);
   app.use(express.static(path.resolve(__dirname, "../public")));
   app.use(cookieParser());
 
