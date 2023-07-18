3ba614a4eee2c9310958379f9835836c78acf0e6
Olamilekan <nunuolamilekan@gmail.com>
2023-07-18 09:22:30 +0100

    updated redis connection

--- server/src/context.ts
+++ server/src/context.ts
@@ -10,12 +10,16 @@ interface Props {
   res: Response;
   req: Request;
 }
-const redis = new Redis({
-  // username: config.redis_username,
-  password: config.redis_password,
-  host: config.redis_host,
-  port: config.redis_port,
-});
+
+const options =
+  process.env.NODE_ENV !== "production"
+    ? {
+        password: config.redis_password,
+        host: config.redis_host,
+        port: config.redis_port,
+      }
+    : config.redis_uri;
+const redis = new Redis(options as any);
 
 const context = async ({ req, res }: Props): Promise<Context> => {
   const user = await authenticated(req);
